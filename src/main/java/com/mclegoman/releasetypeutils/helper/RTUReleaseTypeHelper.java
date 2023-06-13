ea9e80ca4eab004a0306d7b5c7b05765ac077c27
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-06-13 22:53:04 +1200

    1.0.0 - Added Release Type Helper

---
+++ src/main/java/com/mclegoman/releasetypeutils/helper/RTUReleaseTypeHelper.java
@@ -0,0 +1,39 @@
+package com.mclegoman.releasetypeutils.helper;
+
+import com.mclegoman.releasetypeutils.util.RTUTranslations;
+import com.mclegoman.releasetypeutils.util.RTUReleaseTypes;
+import com.mclegoman.releasetypeutils.util.RTUTranslationTypes;
+import net.minecraft.text.Text;
+
+public class RTUReleaseTypeHelper {
+    public static Text getText(RTUReleaseTypes releaseType, RTUTranslationTypes codeType) {
+        String key = ("releasetypeutils.release_type." + releaseType.toString().toLowerCase() + "." + codeType.toString().toLowerCase());
+        return Text.translatable(key);
+    }
+    public static String getString(RTUReleaseTypes releaseType, RTUTranslationTypes codeType) {
+        if (releaseType.equals(RTUReleaseTypes.ALPHA)) {
+            if (codeType.equals(RTUTranslationTypes.CODE)) return RTUTranslations.RT_ALPHA_CODE;
+            else if (codeType.equals(RTUTranslationTypes.SENTENCED)) return RTUTranslations.RT_ALPHA_SENTENCED;
+            else if (codeType.equals(RTUTranslationTypes.NORMAL)) return RTUTranslations.RT_ALPHA_NORMAL;
+        }
+        if (releaseType.equals(RTUReleaseTypes.BETA)) {
+            if (codeType.equals(RTUTranslationTypes.CODE)) return RTUTranslations.RT_BETA_CODE;
+            else if (codeType.equals(RTUTranslationTypes.SENTENCED)) return RTUTranslations.RT_BETA_SENTENCED;
+            else if (codeType.equals(RTUTranslationTypes.NORMAL)) return RTUTranslations.RT_BETA_NORMAL;
+        }
+        if (releaseType.equals(RTUReleaseTypes.RELEASE_CANDIDATE)) {
+            if (codeType.equals(RTUTranslationTypes.CODE)) return RTUTranslations.RT_RELEASE_CANDIDATE_CODE;
+            else if (codeType.equals(RTUTranslationTypes.SENTENCED)) return RTUTranslations.RT_RELEASE_CANDIDATE_SENTENCED;
+            else if (codeType.equals(RTUTranslationTypes.NORMAL)) return RTUTranslations.RT_RELEASE_CANDIDATE_NORMAL;
+        }
+        if (releaseType.equals(RTUReleaseTypes.RELEASE)) {
+            if (codeType.equals(RTUTranslationTypes.CODE)) return RTUTranslations.RT_RELEASE_CODE;
+            else if (codeType.equals(RTUTranslationTypes.SENTENCED)) return RTUTranslations.RT_RELEASE_SENTENCED;
+            else if (codeType.equals(RTUTranslationTypes.NORMAL)) return RTUTranslations.RT_RELEASE_NORMAL;
+        }
+        return null;
+    }
+    public static boolean isDevelopment(RTUReleaseTypes releaseType) {
+        return !releaseType.equals(RTUReleaseTypes.RELEASE);
+    }
+}
---
+++ src/main/java/com/mclegoman/releasetypeutils/util/RTUTranslations.java
@@ -0,0 +1,17 @@
+package com.mclegoman.releasetypeutils.util;
+
+public class RTUTranslations {
+    public static final String NAME = "ReleaseTypeUtils";
+    public static final String RT_ALPHA_CODE = "ALPHA";
+    public static final String RT_ALPHA_SENTENCED = "an alpha";
+    public static final String RT_ALPHA_NORMAL = "alpha";
+    public static final String RT_BETA_CODE = "ALPHA";
+    public static final String RT_BETA_SENTENCED = "an alpha";
+    public static final String RT_BETA_NORMAL = "alpha";
+    public static final String RT_RELEASE_CANDIDATE_CODE = "RELEASE_CANDIDATE";
+    public static final String RT_RELEASE_CANDIDATE_SENTENCED = "a release candidate";
+    public static final String RT_RELEASE_CANDIDATE_NORMAL = "rc";
+    public static final String RT_RELEASE_CODE = "RELEASE";
+    public static final String RT_RELEASE_SENTENCED = "a release";
+    public static final String RT_RELEASE_NORMAL = "release";
+}
