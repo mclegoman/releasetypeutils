1ed0d32990d2bc05a9f83e48bf1bcd65fa12b8e6
dannytaylor <daniel@mclegoman.com>
2024-11-24 19:22:47 +1300

    2.2.0
    It's now recommended to just include the parts of RTU that you need in your project directly.

--- src/main/java/com/mclegoman/releasetypeutils/common/data/Data.java
+++
@@ -1,8 +0,0 @@
-package com.mclegoman.releasetypeutils.common.data;
-
-import com.mclegoman.releasetypeutils.common.version.Helper;
-import com.mclegoman.releasetypeutils.common.version.Version;
-
-public class Data {
-    public static final Version RTU_VERSION = new Version("RTU", "releasetypeutils", 2, 1, 0, Helper.ReleaseType.RELEASE, 1);
-}
